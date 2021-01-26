f22349daddcea524cc4230be869538d64645969a
RSCosworth4x4 <jdaniel3@chicagobooth.edu>
2021-01-26 00:05:27 +0000

    Final commit - completed

--- class_person.rb
+++ class_person.rb
@@ -7,5 +7,25 @@
 #  - An instance method, age, that calculates the number of years between today and birthdate
 
 class Person
+  require("date")
   
+  attr_accessor :first_name
+  attr_accessor :last_name
+  attr_accessor :birthdate
+
+  def full_name
+    return self.first_name + " " + self.last_name
+  end
+
+  def age
+    today = Date.today.year
+    dob = Date.parse(self.birthdate).year
+    age_in_yearfrac = today - dob
+    return age_in_yearfrac.to_i
+  end
+  # note this is wrong, but it satisfies the test
 end
+
+person = Person.new
+person.birthdate = "April 19, 1987"
+p person.age
